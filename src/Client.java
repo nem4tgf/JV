cc79c5b5b6b41014aca9be15e515d34dcc8ac1d9
nem4tgf <143236099+nem4tgf@users.noreply.github.com>
2024-03-21 10:21:25 +0700

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -10,11 +10,14 @@ public class Client {
     public static void main(String[]agrs){
         Student studentA;
         Student studentB;
+        Student studentC;
         studentA=new Student();
         studentB=new Student(1,"Nguyen Van A",true);
-
+        studentC = new Student(2, "Le Ho", false);
         studentB.printInfo();
-
+        studentC.printInfo();
+        studentC.setId(3);
+        studentC.printInfo();
         studentB.setName("Nguyen Van B");
         studentB.printInfo();
     }
